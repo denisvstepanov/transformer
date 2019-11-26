18db198a6b9f5679da957512d119b1e229b62d9f
Denis Stepanov <denis.stepanov@jetbrains.com>
2019-11-26 15:36:38 +0300

    Add debug prints

--- main.py
+++ main.py
@@ -363,9 +363,9 @@ def attention(query, key, value, mask=None, dropout=None):
     d_k = query.size(-1)
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
     if mask is not None:
-        print(f'query: {query.size()} key: {key.size()} value: {value.size()} mask: {mask.size()}')
+        print(f'query: {query.size()} key: {key.size()} value: {value.size()} mask: {mask.size()} scores: {scores.size()}')
         scores = scores.masked_fill(mask == 0, -1e9)
-    p_attn = F.softmax(scores, dim = -1)
+    p_attn = F.softmax(scores, dim=-1)
     if dropout is not None:
         p_attn = dropout(p_attn)
     return torch.matmul(p_attn, value), p_attn
